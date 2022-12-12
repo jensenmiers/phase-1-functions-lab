a198fd5e32b4bd572ac5f77849addcf04cb54993
jensenmiers <jensenmiers@gmail.com>
2022-12-12 15:49:28 -0800

    commit tree aca5d8643031158a2c848b2d0ca24abbe4e3e8be

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+//     if (pickupBlock >= 42 ) {
+//         return pickupBlock - 42
+// }   else {
+//         return 42 - pickupBlock
+// }
+    return Math.abs(pickupBlock - 42)
+}
+
+function distanceFromHqInFeet(start) {
+//     return Math.abs((start - 42)*264)
+// 
+    return distanceFromHqInBlocks(start) * 264
+}   
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264)
+}
+
+function calculatesFarePrice(start,destination) {
+    const totalDistance = distanceTravelledInFeet(start,destination)
+    if (totalDistance <= 400) {
+        return 0
+    }
+    else if (totalDistance > 400 && totalDistance <= 2000) {
+        return (totalDistance - 400) * .02
+    } 
+    else if (totalDistance > 2000 && totalDistance <= 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far' }
+}
